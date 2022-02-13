3af67a98e2151b0e4cbe0c6e36968de3330566f1
NickSagan <9952728@gmail.com>
2022-02-13 15:15:08 +0300

    Initial Commit

---
+++ Simbirsoft DairyTests/Simbirsoft_DairyTests.swift
@@ -0,0 +1,33 @@
+//
+//  Simbirsoft_DairyTests.swift
+//  Simbirsoft DairyTests
+//
+//  Created by Nick Sagan on 13.02.2022.
+//
+
+import XCTest
+@testable import Simbirsoft_Dairy
+
+class Simbirsoft_DairyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
