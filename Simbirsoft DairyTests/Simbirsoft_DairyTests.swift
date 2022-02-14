6f83c01e68be91f832a8df84ff4ec8ac9e074a7e
NickSagan <9952728@gmail.com>
2022-02-14 16:42:10 +0300

    add UNIT TESTS

--- Simbirsoft DairyTests/Simbirsoft_DairyTests.swift
+++ Simbirsoft DairyTests/Simbirsoft_DairyTests.swift
@@ -10,24 +10,34 @@ import XCTest
 
 class Simbirsoft_DairyTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testAdapterGetTasksFromEvents() throws {
+        let adapter = Adapter()
+        let testEvent = EventModel()
+        testEvent.id = 42
+        testEvent.text = "My task 555%!-="
+        testEvent.description = "Описание моего дела Name 999%!-="
+        testEvent.dateInterval = DateInterval(start: Date(timeIntervalSince1970: 147600000), end: Date(timeIntervalSince1970: 147610000))
+        let result = adapter.getTasks(from: [testEvent])
+         
+        XCTAssertNotNil(result[0])
+        XCTAssertTrue(result.count == 1)
+        XCTAssertEqual(result[0].name, "My task 555%!-=")
+        XCTAssertEqual(result[0].description, "Описание моего дела Name 999%!-=")
+        XCTAssertEqual(result[0].id, 42)
+        XCTAssertEqual(result[0].date_start, 147600000)
+        XCTAssertEqual(result[0].date_finish, 147610000)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testAdapterGetEventsFromTasks() {
+        let adapter = Adapter()
+        let testTask = TaskModel(id: 11, date_start: 147600008, date_finish: 147610008, name: " Task In English 123-=!@#$%ˆ&*()_", description: " описание на русском 123-=!@#$%ˆ&*()_")
+        let result = adapter.getEvents(from: [testTask, testTask, testTask])
+         
+        XCTAssertNotNil(result[2])
+        XCTAssertTrue(result.count == 3)
+        XCTAssertEqual(result[0].text, " Task In English 123-=!@#$%ˆ&*()_")
+        XCTAssertEqual(result[1].description, " описание на русском 123-=!@#$%ˆ&*()_")
+        XCTAssertEqual(result[2].id, 11)
+        XCTAssertEqual(result[0].dateInterval, DateInterval(start: Date(timeIntervalSince1970: 147600008), end: Date(timeIntervalSince1970: 147610008)))
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
