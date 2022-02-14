d4dd58e164c50a92479383988bf6dfb27bead352
NickSagan <9952728@gmail.com>
2022-02-14 14:30:36 +0300

    refactor calendarVC

--- Simbirsoft Dairy/Controllers/CalendarVC.swift
+++ Simbirsoft Dairy/Controllers/CalendarVC.swift
@@ -10,37 +10,32 @@ import CalendarKit // https://cocoapods.org/pods/CalendarKit
 
 class CalendarVC: DayViewController {
 
-    var tasks: [TaskModel] = [TaskModel(id: 1, date_start: 1644775438, date_finish: 1644776438, name: "Дело Номер 1", description: "Подробное описание дела номер 1"), TaskModel(id: 2, date_start: 1644851038, date_finish: 1644861038, name: "Дело Номер 2", description: "Подробное описание дела номер 2")]
     var events = [EventModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Simbirsoft Dairy"
+        view.backgroundColor = .systemBackground
+
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addEvent))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        events = DataManager.shared.events
+        for event in events {
+            print(event.id, event.text)
+        }
+        reloadData()
     }
     
     @objc func addEvent() {
         let atvc = AddTaskVC()
-        atvc.modalPresentationStyle = .pageSheet
-        navigationController?.present(atvc, animated: true, completion: nil)
-        
-//        let event = EventModel()
-//        event.id = 1
-//        event.dateInterval = DateInterval(start: Date(timeIntervalSince1970: 1644851038), end: Date(timeIntervalSince1970: 1644869312))
-//        event.text = "Дело 15"
-//        event.description = "Very important task to do asap Very important task to do asap Very important task to do asap Very important task to do asap Very important task to do asap Very important task to do asap Very important task to do asap Very important task to do asap Very important task to do asap Very important task to do asap Very important task to do asapVery important task to do asap"
-//        events.append(event)
-//        print("Add event: \(event)")
-//        reloadData()
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        events = DataManager.shared.events
+        navigationController?.pushViewController(atvc, animated: true)
     }
     
     // Подгружаем список дел на заданный день
     override func eventsForDate(_ date: Date) -> [EventDescriptor] {
-        events
+        return events
     }
 
     // Обрабатываем нажатие на дело
@@ -53,7 +48,6 @@ class CalendarVC: DayViewController {
     private func presentDetailViewForEvent(_ event: EventModel) {
         let dvc = DetailVC()
         dvc.event = event
-        dvc.modalPresentationStyle = .pageSheet
-        navigationController?.present(dvc, animated: true, completion: nil)
+        navigationController?.pushViewController(dvc, animated: true)
     }
 }
