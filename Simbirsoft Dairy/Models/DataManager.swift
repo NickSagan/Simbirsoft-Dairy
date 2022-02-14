9195efc58c333105b57f25230a763d6a6b3b3d15
NickSagan <9952728@gmail.com>
2022-02-14 10:02:25 +0300

    apply adapter in DataManager

--- Simbirsoft Dairy/Models/DataManager.swift
+++ Simbirsoft Dairy/Models/DataManager.swift
@@ -10,16 +10,28 @@ import Foundation
 class DataManager {
 
     static let shared = DataManager()
+    let adapter = Adapter()
     
     private init() { }
     
-    var tasks: [TaskModel] {
+    // Simbirsoft JSON task files type
+    private var tasks: [TaskModel] {
         get {
             guard let data = try? Data(contentsOf: .tasks) else { return [] }
             return (try? JSONDecoder().decode([TaskModel].self, from: data)) ?? []
         }
         set {
             try? JSONEncoder().encode(newValue).write(to: .tasks)
+        }
+    }
+    
+    // CalendarKit event type
+    var events: [EventModel] {
+        get {
+            adapter.getEvents(from: tasks)
+        }
+        set {
+            tasks = adapter.getTasks(from: newValue)
         }
     }
 }
