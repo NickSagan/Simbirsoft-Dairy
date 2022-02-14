71b6ab9745205ee110dd50d3ab8f258c26d3ee8a
NickSagan <9952728@gmail.com>
2022-02-14 14:29:09 +0300

    add Activity View Controller - Share you task

--- Simbirsoft Dairy/Controllers/DetailVC.swift
+++ Simbirsoft Dairy/Controllers/DetailVC.swift
@@ -11,12 +11,15 @@ class DetailVC: UIViewController {
     
     var detailView: DetailView!
     var event: EventModel!
+    private var ac: UIActivityViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         addDetailView()
         displayEvent()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(performActivity))
     }
     
     func addDetailView() {
@@ -36,5 +39,13 @@ class DetailVC: UIViewController {
         detailView.start.text = "Start: \(event.dateInterval.start.dateString())"
         detailView.finish.text = "Finish: \(event.dateInterval.end.dateString())"
         detailView.descriptionText.text = event.description
+    }
+    
+    // Share your task with friends
+    @objc private func performActivity() {
+        let text = "Task: \(event.text)\nDescription: \(event.description)\nStart date: \(event.dateInterval.start.dateString())\nFinish date: \(event.dateInterval.end.dateString())"
+        ac = UIActivityViewController(activityItems: [text], applicationActivities: nil)
+        ac.excludedActivityTypes = [.airDrop, .addToReadingList, .openInIBooks, .saveToCameraRoll]
+        present(ac, animated: true)
     }
 }
